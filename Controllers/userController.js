7db5ceedcdc35f6add4e0a05e13317cd478e79e8
ansaralyh <ansaralyh@gmail.com>
2024-01-22 21:17:47 +0500

    Authentication done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,6 +2,9 @@ const userSchema = require('../Models/user.model');
 const bcrypt = require('bcrypt');
 const jsonwebtoken = require('jsonwebtoken')
 const SECRET = '12345';
+const nodemailer = require('nodemailer')
+const crypto = require('crypto')
+const Otp = require('../Models/otp.model')
 
 
 
@@ -23,6 +26,85 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: "Error saving user" });
     }
 };
+
+// GetSingleUser
+exports.getSingleUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const singleUser = await userSchema.findById(userId);
+        if (!singleUser) {
+            return res.status(404).json({ message: `User with this ${userId} not found` });
+        }
+        res.status(200).json({ data: singleUser });
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+
+// Get all users
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await userSchema.find();
+        if (!users) {
+            return res.status(404).json({
+                success: false,
+                messege: "Users not found"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Users found successfully",
+            data: users
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+}
+
+exports.removeUser = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const removedUser = await userSchema.deleteOne({ _id: userId });
+      res.status(201).json({
+        message: 'User deleted',
+        user:removedUser
+      })
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({
+        message: "Internal Server Error"
+      });
+    }
+  }
+
+
+  exports.updateUser = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const userDetails = req.body;
+      const updatedUser = await userSchema.findByIdAndUpdate(userId, userDetails);
+      if (!updatedUser) {
+        return res.status(401).json({
+          message: "User not found"
+        })
+      }
+      res.status(201).json({
+        message: 'User Updated Successfully',
+        updatedUser
+      })
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({
+        message: "Internal Server Error"
+      });
+    }
+  }
 // login function
 
 exports.login = async (req, res) => {
@@ -64,51 +146,83 @@ exports.login = async (req, res) => {
     }
 };
 
-
-// Get all users
-
-exports.getAllUsers = async (req, res) => {
+exports.forgetPasword = async (req, res) => {
     try {
-        const users = await userSchema.find();
-        if (!users) {
-            return res.status(404).json({
-                success: false,
-                messege: "Users not found"
-            })
-        }
-        return res.status(200).json({
-            success: true,
-            message: "Users found successfully",
-            data: users
-        })
+      const { email } = req.body;
+      const user = await userSchema.findOne({ email });
+
+    //   console.log(user)
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'ansaralyh@gmail.com',
+          pass: 'rbxi feyw kbix rjkd',
+        },
+      });
+  
+      const generatedOTP = crypto.randomBytes(3).toString('hex');
+      console.log(generatedOTP);
+  
+      const mailOptions = {
+        from: 'ansaralyh@gmail.com',
+        to: email,
+        subject: 'Password Reset OTP',
+        text: `Your OTP is ${generatedOTP}`,
+      };
+  
+      await transporter.sendMail(mailOptions);
+  
+      const storedOTP = new Otp({
+        otp: generatedOTP,
+        email,
+      });
+      await storedOTP.save();
+  
+      res.status(200).json({ message: 'OTP sent successfully' });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message: "Server error"
-        });
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+  };
 
 
-
-// GetSingleUser
-exports.getSingleUser = async (req, res) => {
+  exports.verifyOtp = async (req, res) => {
     try {
-        const userId = req.params.id;
-        // console.log("userId :",userId)
-        const singleUser = await userSchema.findById(userId);
-        // console.log(singleUser)
-
-
-
-        if (!singleUser) {
-            return res.status(404).json({ message: `User with this ${userId} not found` });
-        }
-
-        res.status(200).json({ data: singleUser });
+      const { email, otp, newPassword } = req.body;
+      const user = await userSchema.findOne({ email });
+  
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const storedOtp = await Otp.findOne({ email, otp });
+  
+      if (!storedOtp) {
+        return res.status(400).json({ error: 'Invalid OTP' });
+      }
+  
+      if (user) {
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+  
+        await user.save();
+        await storedOtp.deleteOne();
+  
+        return res.status(200).json({ message: 'Password updated successfully' });
+      } else {
+        return res.status(400).json({ error: 'User not verified' });
+      }
     } catch (error) {
-        console.error("Error fetching user:", error);
-        res.status(500).json({ message: "Internal Server Error" });
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-};
+  };
+
+
+
 
--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -6,6 +6,11 @@ router.post('/user/register', userController.register);
 router.post('/user/login', userController.login);
 router.get('/user/view/:id', userController.getSingleUser);
 router.get('/user/view', userController.getAllUsers);
+router.delete('/user/:id', userController.removeUser);
+router.put('/user/:id', userController.updateUser);
+router.post('/user/forgetPassword', userController.forgetPasword);
+router.post('/user/verifyOtp', userController.verifyOtp);
+
 
 
 module.exports = router
